74627ce1f55f573628a0a0c0291f84d5f11e81dc
malsamax <malsamax@protonmail.com>
2023-11-12 21:22:40 +0000

    add some mobile styling

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,19 +6,19 @@ export default function Home() {
   const prompt = "Please choose an option:";
   const options = [
     { text: "CV", route: "cv" },
-    { text: "About Me", route: "about" },
-    { text: "Projects", route: "projects" },
+    { text: "About Me", route: "cv" },
+    { text: "Projects", route: "cv" },
   ];
   return (
     <main className={`${p2p.className} flex min-h-screen flex-col p-6 `}>
       <div className="mt-4 flex grow flex-col items-center justify-center gap-4 md:flex-col ">
-        <h1 className="pb-12 text-6xl">Sam Clein</h1>
-        <GameText>{prompt}</GameText>
-        <div className="flex flex-col items-start">
+        <h1 className="pb-1 text-6xl">Sam Clein</h1>
+        <GameText className="min-h-[48px]">{prompt}</GameText>
+        <div className="flex min-h-[75px] flex-col items-start gap-3 md:gap-0">
           {options.map((option, index) => (
             <Link
               href={option.route}
-              key={option.route}
+              key={option.text}
               className={
                 "outline-none hover:text-blue-300 focus:animate-pulse focus:text-blue-500 focus:animate-duration-500"
               }
